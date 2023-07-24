8eeb57c03396c505ce7ba2276f8fe0a5b08bb615
Roland Tanglao <roland@rolandtanglao.com>
2023-07-23 22:40:13 -0700

    change from RGB to RGBA

--- create-question-answer-vt100graphics.rb
+++ create-question-answer-vt100graphics.rb
@@ -13,66 +13,61 @@ logger.level = Logger::DEBUG
 # From https://simplicable.com/colors/dark-pink "64 Types of Dark Pink"
 PINK_COLOUR_PALETTE =
   %w[
-    #7c383e #8c055e #90305d #985672 #9c004a #9d6984 #ab485b #ab495c
-    #b94c66 #bb3377 #c6174e #c62168 #c62d42 #c7607b #c93756 #c95efb
-    #c97376 #cb416b #cc338b #cc33cc #cd7584 #ce6ba4 #d0417e #d04a70
-    #d0576b #d2738f #d3507a #d46a7e #d58d8a #d74894 #d99294 #d998a0
-    #da467d #da6d91 #db4bda #dd00cc #dd8374 #df6fa1 #e04f80 #e34285
-    #e4007c #e4445e #e94b7e #e95295 #e96a97 #ea5a79 #eb4962 #ec6d71
-    #f504c9 #f5054f #f56991 #f62681 #f77fbe #fa6e79 #fc0fc0 #fe02a2
-    #fe46a5 #ff007e #ff0090 #ff0099 #ff0789 #ff1476 #ff1493 #ff787b
+    #7c383eff #8c055eff #90305dff #985672ff #9c004aff #9d6984ff #ab485bff #ab495cff
+    #b94c66ff #bb3377ff #c6174eff #c62168ff #c62d42ff #c7607bff #c93756ff #c95efbff
+    #c97376ff #cb416bff #cc338bff #cc33ccff #cd7584ff #ce6ba4ff #d0417eff #d04a70ff
+    #d0576bff #d2738fff #d3507aff #d46a7eff #d58d8aff #d74894ff #d99294ff #d998a0ff
+    #da467dff #da6d91ff #db4bdaff #dd00ccff #dd8374ff #df6fa1ff #e04f80ff #e34285ff
+    #e4007cff #e4445eff #e94b7eff #e95295ff #e96a97ff #ea5a79ff #eb4962ff #ec6d71ff
+    #f504c9ff #f5054fff #f56991ff #f62681ff #f77fbeff #fa6e79ff #fc0fc0ff #fe02a2ff
+    #fe46a5ff #ff007eff #ff0090ff #ff0099ff #ff0789ff #ff1476ff #ff1493ff #ff787bff
   ].freeze
-WHITE = '#ffffff'.freeze
+TRANSPARENT = '#00000000'.freeze
 SWEET_CANYON_PALETTE =
   %w[
-    #0f0e11 #2d2c33 #40404a #51545c #6b7179 #7c8389 #a8b2b6 #d5d5d5
-    #eeebe0 #f1dbb1 #eec99f #e1a17e #cc9562 #ab7b49 #9a643a #86482f
-    #783a29 #6a3328 #541d29
-    #42192c
-    #512240
-    #782349
-    #8b2e5d
-    #a93e89
-    #d062c8
-    #ec94ea
-    #f2bdfc
-    #eaebff
-    #a2fafa
-    #64e7e7
-    #54cfd8
-    #2fb6c3
-    #2c89af
-    #25739d
-    #2a5684
-    #214574
-    #1f2966
-    #101445
-    #3c0d3b
-    #66164c
-    #901f3d
-    #bb3030
-    #dc473c
-    #ec6a45
-    #fb9b41
-    #f0c04c
-    #f4d66e
-    #fffb76
-    #ccf17a
-    #97d948
-    #6fba3b
-    #229443
-    #1d7e45
-    #116548
-    #0c4f3f
-    #0a3639
-    #251746
-    #48246d
-    #69189c
-    #9f20c0
-    #e527d2
-    #ff51cf
-    #ff7ada
-    #ff9edb
+    #0f0e11ff #2d2c33ff #40404aff #51545cff #6b7179ff #7c8389ff #a8b2b6ff #d5d5d5ff
+    #eeebe0ff #f1dbb1ff #eec99fff #e1a17eff #cc9562ff #ab7b49ff #9a643aff #86482fff
+    #783a29ff #6a3328ff #541d29ff #42192cff #512240ff #782349ff #8b2e5dff #a93e89ff
+    #d062c8ff
+    #ec94eaff
+    #f2bdfcff
+    #eaebffff
+    #a2fafaff
+    #64e7e7ff
+    #54cfd8ff
+    #2fb6c3ff
+    #2c89afff
+    #25739dff
+    #2a5684ff
+    #214574ff
+    #1f2966ff
+    #101445ff
+    #3c0d3bff
+    #66164cff
+    #901f3dff
+    #bb3030ff
+    #dc473cff
+    #ec6a45ff
+    #fb9b41ff
+    #f0c04cff
+    #f4d66eff
+    #fffb76ff
+    #ccf17aff
+    #97d948ff
+    #6fba3bff
+    #229443ff
+    #1d7e45ff
+    #116548ff
+    #0c4f3fff
+    #0a3639ff
+    #251746ff
+    #48246dff
+    #69189cff
+    #9f20c0ff
+    #e527d2ff
+    #ff51cfff
+    #ff7adaff
+    #ff9edbff
   ].freeze
 
 def add_colours_based_on_text(palette, pixels, text)
@@ -85,7 +80,7 @@ def add_colours_based_on_text(palette, pixels, text)
   # if it's not a multiple of 80 characters, pad with white
   return pixels if text_length_mod80.zero?
 
-  pixels += Array.new(80 - text_length_mod80) { WHITE }
+  pixels += Array.new(80 - text_length_mod80) { TRANSPARENT }
 end
 
 if ARGV.length < 2
@@ -96,17 +91,21 @@ end
 all_questions = CSV.read(ARGV[0], headers: true)
 all_answers = CSV.read(ARGV[1], headers: true)\
 
-fn_str = 'tb-question-colours-%<id>s-%<yyyy>4.4d-%<mm>2.2d-%<dd>2.2d-%<hh>2.2d-%<min>2.2d-%<ss>2.2d.text'
+fn_str = 'tb-question-colours-%<id>s-%<yyyy>4.4d-%<mm>2.2d-%<dd>2.2d-%<hh>2.2d-'
+fn_str += '%<min>2.2d-%<ss>2.2d-80x%<num_rows>d.text'
 
 all_questions.each do |q|
   pixels = []
   id = q['id']
   pixels = add_colours_based_on_text(PINK_COLOUR_PALETTE, pixels, q['title'])
   pixels = add_colours_based_on_text(PINK_COLOUR_PALETTE, pixels, q['content'])
+  num_rows = 2
   question_creator = q['creator']
   created = Time.parse(q['created']).utc
   answers = all_answers.find_all { |a| a['question_id'] == id }
   answers.each do |a|
+    length = a['content'].length
+    num_rows += length % 80 == 0 ? length.div(80) : length.div(80) + 1
     pixels = if a['creator'] == question_creator
                add_colours_based_on_text(PINK_COLOUR_PALETTE, pixels, a['content'])
              else
@@ -116,7 +115,7 @@ all_questions.each do |q|
   filename = format(
     fn_str,
     id: id, yyyy: created.year, mm: created.month, dd: created.day,
-    hh: created.hour, min: created.min, ss: created.sec
+    hh: created.hour, min: created.min, ss: created.sec, num_rows: num_rows
   )
   File.open(filename, 'w+') do |f|
     f.puts(pixels)
